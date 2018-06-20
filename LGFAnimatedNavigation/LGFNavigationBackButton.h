59b2e9337890e140c14e97e72708eb94c23e10cb
452354033@qq.com <452354033@qq.com>
2018-06-20 15:14:43 +0800

    第一版

---
+++ LGFAnimatedNavigation/LGFNavigationBackButton.h
@@ -0,0 +1,14 @@
+//
+//  LGFNavigationBackButton.h
+//  LGFOCTool
+//
+//  Created by apple on 2018/6/19.
+//  Copyright © 2018年 来国锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGFNavigationBackButton : UIButton
+- (instancetype)initWithCoder:(NSCoder *)aDecoder;
+- (instancetype)initWithFrame:(CGRect)frame;
+@end
